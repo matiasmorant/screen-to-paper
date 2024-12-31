1d7b26767121acb782860d39e0d7da4350e431f3
matiasmorant <matias.morant.mm@gmail.com>
2024-12-31 12:25:52 -0300

    pwa

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'screen-to-paper-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.png',
+  '/manifest.json',
+  '/styles.css', // Include your CSS file if available
+  '/script.js',  // Include your main JavaScript file if available
+  'https://unpkg.com/pdf-lib@1.17.1/dist/pdf-lib.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
